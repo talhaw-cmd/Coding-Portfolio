e5972e51519e5a72fd7ded5a20c645d9d2994148
talhaw-cmd <123mtalha789@gmail.com>
2026-01-14 17:00:47 +0500

    Portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import "./index.css"
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
+import React from 'react'
 
 createRoot(document.getElementById('root')).render(
-  <App/>
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>,
 )
