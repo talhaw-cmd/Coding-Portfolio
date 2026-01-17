6f0788b7a0549ab438243f902abc490a13edb2e6
talhaw-cmd <123mtalha789@gmail.com>
2026-01-17 18:30:28 +0500

    Coding-Portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import "./index.css"
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from "@vercel/analytics/react"
 import React from 'react'
 
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights/>
   </React.StrictMode>,
 )
